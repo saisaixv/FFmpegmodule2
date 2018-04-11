32cb68473c0e869b68b8b69e07e3ef5aea088c69
saisai <47806328@qq.com>
2018-04-11 23:08:36 +0800

    添加了surfaceview显示视频

--- app/src/main/java/com/shunyi/ffmpegmodule/MainActivity.java
+++ app/src/main/java/com/shunyi/ffmpegmodule/MainActivity.java
@@ -1,10 +1,15 @@
 package com.shunyi.ffmpegmodule;
 
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,32 +18,81 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("ffmpegmodule");
     }
 
+    private SurfaceView surfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI()+"");
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI()+"");
         final String dir= Environment.getExternalStorageDirectory().getAbsolutePath()+"/";
+
+        surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        final SurfaceHolder holder = surfaceView.getHolder();
+//        holder.addCallback(new SurfaceHolder.Callback() {
+//            @Override
+//            public void surfaceCreated(final SurfaceHolder holder) {
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        main(dir+"/独角戏.avi",
+//                                dir+"/三个音轨.h264",
+//                                dir+"/三个音轨.aac",
+//                                holder);
+//
+//                    }
+//                }).start();
+//            }
+//
+//            @Override
+//            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+//
+//            }
+//
+//            @Override
+//            public void surfaceDestroyed(SurfaceHolder holder) {
+//
+//            }
+//        });
         findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        main(dir+"/三个音轨.ts",dir+"/三个音轨.h264",dir+"/三个音轨.aac");
+                        main(dir+"/三个音轨.ts",
+                                dir+"/三个音轨.h264",
+                                dir+"/三个音轨.aac",
+                                holder.getSurface());
                     }
                 }).start();
             }
         });
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int mSurfaceViewWidth = dm.widthPixels;
+        int mSurfaceViewHeight = dm.heightPixels;
+
+        int h = 1080 * 1080 / (1920-66);
+        int margin = (mSurfaceViewHeight - h) / 2;
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT);
+        lp.setMargins(0,margin, 0, margin);
+        surfaceView.setLayoutParams(lp);
+
     }
+
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native int stringFromJNI();
-    public native int main(String fileName,String fileName_V,String fileName_A);
+    public native int main(String fileName,String fileName_V,String fileName_A,Object surface);
 }
--- app/src/main/jni/ffmpegmodule.c
+++ app/src/main/jni/ffmpegmodule.c
@@ -7,6 +7,11 @@
 #include "include/libavformat/avformat.h"
 #include <android/log.h>
 #include "include/libavutil/mathematics.h"
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+#include "include/libavutil/avutil.h"
+#include "include/libavutil/imgutils.h"
+#include "include/libswscale/swscale.h"
 
 JNIEXPORT jint JNICALL Java_com_shunyi_ffmpegmodule_MainActivity_stringFromJNI(
         JNIEnv *env,
@@ -96,65 +101,233 @@ jint init_demuxer(){
 
 jint Java_com_shunyi_ffmpegmodule_MainActivity_main(
         JNIEnv *env,
-        jobject jOB,jstring filename,jstring filename_v,jstring filename_a){
+        jobject jOB,jstring filename,jstring filename_v,jstring filename_a,jobject surface){
+
+    __android_log_print(ANDROID_LOG_ERROR,"main","进来了");
+    //获取native window
+    ANativeWindow *nativeWindow=ANativeWindow_fromSurface(env,surface);
+    __android_log_print(ANDROID_LOG_ERROR,"main","surface");
 
     in_filename=(*env)->GetStringUTFChars(env,filename,0);
     out_filename_v=(*env)->GetStringUTFChars(env,filename_v,0);
     out_filename_a=(*env)->GetStringUTFChars(env,filename_a,0);
 
-    if ((ret = init_demuxer()) < 0)
-//        errReport("init_demuxer", ret);
-        __android_log_print(ANDROID_LOG_ERROR,"jni","error = %d",ret);
-    while (1) {
-        AVFormatContext *ofmt_ctx;
-        AVStream *in_stream, *out_stream;
-        if (av_read_frame(ifmt_ctx, &pkt) < 0)
-            break;
-        in_stream = ifmt_ctx->streams[pkt.stream_index];
-        if (pkt.stream_index == videoindex) {
-            out_stream = ofmt_ctx_v->streams[0];
-            ofmt_ctx = ofmt_ctx_v;
-//            printf("\nv#s:%d\tp:%lld", pkt.size, pkt.pts);
-            __android_log_print(ANDROID_LOG_ERROR,"jni","pkt.size = %d  pts = %lld",pkt.size, pkt.pts);
-        }
-        else if (pkt.stream_index == audioindex) {
-            out_stream = ofmt_ctx_a->streams[0];
-            ofmt_ctx = ofmt_ctx_a;
-            __android_log_print(ANDROID_LOG_ERROR,"jni","pkt.size = %d  pts = %lld",pkt.size, pkt.pts);
-        }
-        else {
-            continue;
-        }
-        pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-        pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-        pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
-        pkt.pos = -1;
-        pkt.stream_index = 0;
+//    if ((ret = init_demuxer()) < 0)
+////        errReport("init_demuxer", ret);
+//        __android_log_print(ANDROID_LOG_ERROR,"jni","error = %d",ret);
+//    while (1) {
+//        AVFormatContext *ofmt_ctx;
+//        AVStream *in_stream, *out_stream;
+//        if (av_read_frame(ifmt_ctx, &pkt) < 0)
+//            break;
+//        in_stream = ifmt_ctx->streams[pkt.stream_index];
+//        if (pkt.stream_index == videoindex) {
+//            out_stream = ofmt_ctx_v->streams[0];
+//            ofmt_ctx = ofmt_ctx_v;
+////            printf("\nv#s:%d\tp:%lld", pkt.size, pkt.pts);
+//            __android_log_print(ANDROID_LOG_ERROR,"jni","pkt.size = %d  pts = %lld",pkt.size, pkt.pts);
+//        }
+//        else if (pkt.stream_index == audioindex) {
+//            out_stream = ofmt_ctx_a->streams[0];
+//            ofmt_ctx = ofmt_ctx_a;
+//            __android_log_print(ANDROID_LOG_ERROR,"jni","pkt.size = %d  pts = %lld",pkt.size, pkt.pts);
+//        }
+//        else {
+//            continue;
+//        }
+//        pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+//        pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+//        pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+//        pkt.pos = -1;
+//        pkt.stream_index = 0;
+//
+//        if (av_interleaved_write_frame(ofmt_ctx, &pkt) < 0) {
+//            __android_log_print(ANDROID_LOG_ERROR,"jni","Error muxing packet");
+//
+//            break;
+//        }
+//        //printf("Write %8d frames to output file\n",frame_index);
+//        av_free_packet(&pkt);
+//        frame_index++;
+//    }
+//    av_write_trailer(ofmt_ctx_a);
+//    av_write_trailer(ofmt_ctx_v);
+//    avformat_close_input(&ifmt_ctx);
+//    if (ofmt_ctx_a && !(ofmt_a->flags & AVFMT_NOFILE))
+//        avio_close(ofmt_ctx_a->pb);
+//    if (ofmt_ctx_v && !(ofmt_v->flags & AVFMT_NOFILE))
+//        avio_close(ofmt_ctx_v->pb);
+//    avformat_free_context(ofmt_ctx_a);
+//    avformat_free_context(ofmt_ctx_v);
+//
+//    if (ret < 0 && ret != AVERROR_EOF) {
+//        __android_log_print(ANDROID_LOG_ERROR,"jni","Error occurred");
+//        return -1;
+//    }
+//    printf("successed.\n");
+//    __android_log_print(ANDROID_LOG_ERROR,"jni","successed");
+//    return 0;
 
-        if (av_interleaved_write_frame(ofmt_ctx, &pkt) < 0) {
-            __android_log_print(ANDROID_LOG_ERROR,"jni","Error muxing packet");
+    av_register_all();
+    __android_log_print(ANDROID_LOG_ERROR,"main","1");
+    AVFormatContext *pFormatCtx = avformat_alloc_context();
 
-            break;
-        }
-        //printf("Write %8d frames to output file\n",frame_index);
-        av_free_packet(&pkt);
-        frame_index++;
+    // Open video file
+    if (avformat_open_input(&pFormatCtx, in_filename, NULL, NULL) != 0) {
+
+//        LOGD("Couldn't open file:%s\n", file_name);
+        __android_log_print(ANDROID_LOG_ERROR,"main","Couldn't open file:%s",in_filename);
+        return -1; // Couldn't open file
     }
-    av_write_trailer(ofmt_ctx_a);
-    av_write_trailer(ofmt_ctx_v);
-    avformat_close_input(&ifmt_ctx);
-    if (ofmt_ctx_a && !(ofmt_a->flags & AVFMT_NOFILE))
-        avio_close(ofmt_ctx_a->pb);
-    if (ofmt_ctx_v && !(ofmt_v->flags & AVFMT_NOFILE))
-        avio_close(ofmt_ctx_v->pb);
-    avformat_free_context(ofmt_ctx_a);
-    avformat_free_context(ofmt_ctx_v);
+    __android_log_print(ANDROID_LOG_ERROR,"main","2");
 
-    if (ret < 0 && ret != AVERROR_EOF) {
-        __android_log_print(ANDROID_LOG_ERROR,"jni","Error occurred");
+    // Retrieve stream information
+    if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
+//        LOGD("Couldn't find stream information.");
+        __android_log_print(ANDROID_LOG_ERROR,"main","Couldn't find stream information");
         return -1;
     }
-    printf("successed.\n");
-    __android_log_print(ANDROID_LOG_ERROR,"jni","successed");
+    __android_log_print(ANDROID_LOG_ERROR,"main","3");
+
+    // Find the first video stream
+    int videoStream = -1, i;
+    for (i = 0; i < pFormatCtx->nb_streams; i++) {
+        if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO
+            && videoStream < 0) {
+            videoStream = i;
+        }
+    }
+    __android_log_print(ANDROID_LOG_ERROR,"main","4");
+    if (videoStream == -1) {
+//        LOGD("Didn't find a video stream.");
+        __android_log_print(ANDROID_LOG_ERROR,"main","Didn't find a video stream");
+        return -1; // Didn't find a video stream
+    }
+    __android_log_print(ANDROID_LOG_ERROR,"main","5");
+
+    // Get a pointer to the codec context for the video stream
+    AVCodecContext *pCodecCtx = pFormatCtx->streams[videoStream]->codec;
+
+    // Find the decoder for the video stream
+    AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+    if (pCodec == NULL) {
+//        LOGD("Codec not found.");
+        __android_log_print(ANDROID_LOG_ERROR,"main","Codec not found");
+        return -1; // Codec not found
+    }
+    __android_log_print(ANDROID_LOG_ERROR,"main","6");
+
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+//        LOGD("Could not open codec.");
+        __android_log_print(ANDROID_LOG_ERROR,"main","Could not open codec");
+        return -1; // Could not open codec
+    }
+    __android_log_print(ANDROID_LOG_ERROR,"main","7");
+
+    // 获取native window
+//    ANativeWindow *nativeWindow = ANativeWindow_fromSurface(env, surface);
+
+    // 获取视频宽高
+    int videoWidth = pCodecCtx->width;
+    int videoHeight = pCodecCtx->height;
+
+    __android_log_print(ANDROID_LOG_ERROR,"main","videoWidth = %d   videoHeight = %d",videoWidth,videoHeight);
+
+    // 设置native window的buffer大小,可自动拉伸
+    ANativeWindow_setBuffersGeometry(nativeWindow, videoWidth, videoHeight,
+                                     WINDOW_FORMAT_RGBA_8888);
+    ANativeWindow_Buffer windowBuffer;
+    __android_log_print(ANDROID_LOG_ERROR,"main","8");
+
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+//        LOGD("Could not open codec.");
+        __android_log_print(ANDROID_LOG_ERROR,"main","Could not open codec");
+        return -1; // Could not open codec
+    }
+    __android_log_print(ANDROID_LOG_ERROR,"main","9");
+
+    // Allocate video frame
+    AVFrame *pFrame = av_frame_alloc();
+
+    // 用于渲染
+    AVFrame *pFrameRGBA = av_frame_alloc();
+    if (pFrameRGBA == NULL || pFrame == NULL) {
+//        LOGD("Could not allocate video frame.");
+        __android_log_print(ANDROID_LOG_ERROR,"main","Could not allocate video frame");
+        return -1;
+    }
+
+    // Determine required buffer size and allocate buffer
+    // buffer中数据就是用于渲染的,且格式为RGBA
+    int numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGBA, pCodecCtx->width, pCodecCtx->height,
+                                            1);
+    uint8_t *buffer = (uint8_t *) av_malloc(numBytes * sizeof(uint8_t));
+    av_image_fill_arrays(pFrameRGBA->data, pFrameRGBA->linesize, buffer, AV_PIX_FMT_RGBA,
+                         pCodecCtx->width, pCodecCtx->height, 1);
+
+    // 由于解码出来的帧格式不是RGBA的,在渲染之前需要进行格式转换
+    struct SwsContext *sws_ctx = sws_getContext(pCodecCtx->width,
+                                                pCodecCtx->height,
+                                                pCodecCtx->pix_fmt,
+                                                pCodecCtx->width,
+                                                pCodecCtx->height,
+                                                AV_PIX_FMT_RGBA,
+                                                SWS_BILINEAR,
+                                                NULL,
+                                                NULL,
+                                                NULL);
+
+    int frameFinished;
+    AVPacket packet;
+    while (av_read_frame(pFormatCtx, &packet) >= 0) {
+        // Is this a packet from the video stream?
+        if (packet.stream_index == videoStream) {
+
+            // Decode video frame
+            avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
+
+            // 并不是decode一次就可解码出一帧
+            if (frameFinished) {
+
+                // lock native window buffer
+                ANativeWindow_lock(nativeWindow, &windowBuffer, 0);
+
+                // 格式转换
+                sws_scale(sws_ctx, (uint8_t const *const *) pFrame->data,
+                          pFrame->linesize, 0, pCodecCtx->height,
+                          pFrameRGBA->data, pFrameRGBA->linesize);
+
+                // 获取stride
+                uint8_t *dst = (uint8_t *) windowBuffer.bits;
+                int dstStride = windowBuffer.stride * 4;
+                uint8_t *src = (pFrameRGBA->data[0]);
+                int srcStride = pFrameRGBA->linesize[0];
+
+                // 由于window的stride和帧的stride不同,因此需要逐行复制
+                int h;
+                for (h = 0; h < videoHeight; h++) {
+                    memcpy(dst + h * dstStride, src + h * srcStride, srcStride);
+                }
+
+                ANativeWindow_unlockAndPost(nativeWindow);
+            }
+
+        }else{
+
+        }
+        av_packet_unref(&packet);
+    }
+
+    av_free(buffer);
+    av_free(pFrameRGBA);
+
+    // Free the YUV frame
+    av_free(pFrame);
+
+    // Close the codecs
+    avcodec_close(pCodecCtx);
+
+    // Close the video file
+    avformat_close_input(&pFormatCtx);
     return 0;
 }
